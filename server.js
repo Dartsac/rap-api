b152d1ec1cbe1bcda6f33136e12451c4dc693697
Dartsac <idobson.gatech@gmail.com>
2022-10-25 21:59:04 -0400

    playing around

--- server.js
+++ server.js
@@ -6,22 +6,21 @@ const PORT = 8000
 app.use(cors())
 
 let rappers = {
-  '21 savage': {
+  '21_savage': {
     age: 28,
     birthName: 'Sheyaa Bin Abraham-Joseph',
     birthLocation: 'London, England',
   },
-  'chance the rapper': {
+  'chance_the_rapper': {
     age: 27,
     birthName: 'Chancelor Jonathan Bennett',
     birthLocation: 'Chicago, Illinois',
   },
-  'unknown': {
+  unknown: {
     age: 'unknown',
     birthName: 'unknown',
     birthLocation: 'unknown',
-
-  }
+  },
 }
 
 app.get('/', (request, response) => {
@@ -31,7 +30,7 @@ app.get('/', (request, response) => {
 app.get('/api/rappers/:rapperName', (request, response) => {
   const rapName = request.params.rapperName.toLowerCase()
   console.log(rapName)
-  if(rappers[rapName]) {
+  if (rappers[rapName]) {
     response.json(rappers[rapName])
   } else {
     response.json(rappers['unknown'])
